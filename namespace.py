74f9c24a8ae8d24bad7328f9abd888cc7c1ddeaa
Graciela Garcia <roxiee.aveley@gmail.com>
2016-03-10 10:48:02 -0500

    cambios a namespace

--- namespace.py
+++ namespace.py
@@ -3,12 +3,21 @@ class NamespaceTable:
     def __init__(self):
         pass
     
+    """metodo que se llama cuando se lee que se esta declarando una funcion en el programa prueba
+    nameOfFunction = nombre de la funcion
+    typeOfFunction = tipo de retorno de la funcion
+    """
     def addFunction(self, nameOfFunction, typeOfFunction):
         print("funcion", nameOfFunction, "de tipo", typeOfFunction, "agregada")
     
+    """metodo que se llama cuando se lee que se esta declarando una variable
+    nameOfFunction es el nombre de la funcion en cuyo cuerpo se esta declarando esa variable"""
     def addVariable(self, nameOfVariable, typeOfVariable, nameOfFunction):
         print("variable", nameOfVariable, "de tipo", typeOfVariable, "agregada en el ambito de", nameOfFunction)
     
+    """metodo que se llama cuando se lee que una funcion recibe parametros
+    si es por referencia, mode vale 'referencia'
+    nameOfFunction es el nombre de la funcion a la que pertenece el parametro"""
     def addParameter(self, nameOfParameter, typeOfParameter, mode, nameOfFunction):
         if (mode == None):
             print("parametro", nameOfParameter, "de tipo", typeOfParameter, "por valor agregado en el ambito de", nameOfFunction)
