f286f249333c44500f025d086eb62f0e035f6f8e
Graciela Garcia <roxiee.aveley@gmail.com>
2016-03-11 08:58:26 -0500

    Cambios a la estructura de datos en namespace.y

--- namespace.py
+++ namespace.py
@@ -1,15 +1,62 @@
+class Variable
+''' Una variable tiene nombre y tipo'''
+	def __init__(self,nameOfVariable,typeOfVariable,)
+		self.name=nameOfVariable;
+		self.type=typeOfVariable;
+
+class Parameter
+'''Un parametro es una variable con tipo de referencia'''
+	def __init__(self,nameOfVariable,typeOfVariable,ref)
+		self.variable = Variable(nameOfVariable,typeOfVariable);
+		self.referenceType = ref;
+
+class Function
+'''Una funcion tiene nombre, tipo y variables, que pueden ser variables o parametros'''
+	def __init__(self,nameOfFunction,typeOfFunction)
+		self.name = nameOfFunction;
+		self.type = typeOfFunction;
+		self.variables = [];
+		
+	def addParameter(self,nameOfVariable,typeOfVariable,ref)
+		auxiliar = Parameter(nameOfVariable,typeOfVariable,ref);
+		self.variables.append(auxiliar);
+	
+	def addVariables
+		auxiliar = Variable(nameOfVariable,typeOfVariable);
+		self.variables.append(auxiliar);	
+		
+		
+
 class NamespaceTable:
     
     def __init__(self):
-        self.tabla = {} 
-    
-    """Metodo queagrega una funcion a la tabla de funciones. 
+        self.tabla = ();
+		
+	def searchFunction(self,name)
+	'''Metodo que busca una funcion en la tabla de funciones MAP LISTA'''
+		for f in self.tabla
+			if(f.name == name)
+				return true;
+		return false;
+	
+	def searchVariable(self,nameOfVariable,nameOfFunction)
+	'''Metodo que busca una variable dentro de una funcion'''
+		for f in self.tabla
+			if(f.name == nameOfFunction)
+				if nameOfVariable in f.variables
+					return true;
+				else
+					return false;
+					
+				
+    """Metodo que agrega una funcion a la tabla de funciones. 
     Regresa True si la operacion fue exitosa, False si no."""
-    def addFunction(self, nameOfFunction, typeOfFunction):
-        if nameOfFunction in self.tabla:
+    def addFunction(self, nameOfFunction, typeOfFunction, numberOfParameters):	
+        if searchFunction(nameOfFunction)
             return False
         else:
-            self.tabla[nameOfFunction] = {}
+			auxiliar = Function(nameOfFunction,typeOfFunction);	
+            self.tabla.append(auxiliar);
             return True
     
     """Metodo que agrega una variable a la tabla de variables en el ambito de la funcion dada.
@@ -18,8 +65,13 @@ class NamespaceTable:
         if nameOfVariable in self.tabla[nameOfFunction]:
             return False
         else:
-            self.tabla[nameOfFunction][nameOfVariable] = typeOfVariable
-            return True
+			auxiliar=Variable(nameOfVariable,typeOfVariable);
+			if nameOfFunction in self.tabla
+				'''ME QUEDE AQUIII'''
+				self.tabla[nameOfFunction][auxiliar.name] = typeOfVariable;
+				return True;
+			else
+				return False;
     
     """metodo que agrega un parametro a la tabla de variables en el ambito de la funcion dada.
     Regresa True si la operacion fue exitosa, False si no."""
