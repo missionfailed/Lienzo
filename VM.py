92aced4cd11483154b9a3694104285f540e3a2b8
Graciela Garcia <roxiee.aveley@gmail.com>
2016-04-13 17:55:30 -0500

    Trabajando en VM.py

--- VM.py
+++ VM.py
@@ -1,8 +1,11 @@
 """Programa que lee el archivo cuadruplos.txt y traduce sus instrucciones"""
 import turtle
 
-f = open('cuadruplos.txt',r)
+f = open('ejemplocuadruplos.txt','r+')
 for line in f:
     s = f.readline();
     """String viene de la forma ('op', 'operando1', 'operando2','memoria')"""
-    lista = s.split("\"",3)
+    lista = s.split(',',4)
+    
+    print(lista[0])
+    
